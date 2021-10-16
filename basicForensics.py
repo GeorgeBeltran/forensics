71372853b7e979083d26397cec8f936abb647088
George <31018576+GeorgeBeltran@users.noreply.github.com>
2021-10-15 22:54:39 -0700

    Delete basicForensics.py

--- basicForensics.py
+++
@@ -1,7 +0,0 @@
-import sys
-import hashlib
-print("Please enter your full name")
-line = sys.stdin.readline()
-line = line.rstrip()
-md5_object = hashlib.md5()
-print(md5_object.hexdigest())   # Prints the output as per the hashing algorithm i.e. md5
